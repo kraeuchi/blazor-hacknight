21392a46da29c6e110e0df8b2d90d311cc4e9931
Tinu <38808437+kraeuchi@users.noreply.github.com>
2020-01-08 19:15:03 +0100

    Example of ToDo List with a Handler Class

---
+++ HouseworkSrv/Data/AppData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseworkManager.Data
+{
+    public class AppData
+    {
+        public int Age { get; set; }
+        public String CurrentNickname { get; set; }
+
+        public int CurrentPointsperweek { get; set; }
+        
+    }
+}
---
+++ HouseworkSrv/Data/MemberData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+
+
+namespace HouseworkManager.Data
+{
+
+
+
+    public class Member
+    {
+        public string Nickname { get; set; } = "";
+        public int PointsPerWeek { get; set; } = 100;
+    }
+
+
+    public class MemberService
+    {
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        //  [Inject]
+        //  public AppData appdata { get; set; }
+
+        public List<Member> members = new List<Member>();
+
+        public List<Member> AddDefaultsAsync()
+        {
+            //  await Http.GetJsonAsync<Member[]>("sample-data/member.json");
+            if (members.Count == 0)
+            {
+                Member x = new Member()
+                {
+                    Nickname = "Martin",
+                    PointsPerWeek = 66
+                };
+
+                members.Add(x);
+
+                members.Add(new Member()
+                {
+                    Nickname = "Nuria",
+                    PointsPerWeek = 76
+                });
+            }
+            return members;
+        }
+
+        public List<Member> Add(String CurrentNickname, int CurrentPointsperweek)
+        {
+            members.Add(new Member()
+            {
+                Nickname = CurrentNickname,
+                PointsPerWeek = CurrentPointsperweek
+            });
+            return members;
+        }
+
+
+    }
+
+
+
+}
+
+
